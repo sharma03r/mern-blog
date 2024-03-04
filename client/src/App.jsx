46e50d1dbbf41d9c61d18cab9589ebbed02c181e
Rahul Sharma <sharmarahul8848@gmail.com>
2024-03-04 19:31:59 +0530

    Created the header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import About from "./pages/About";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
+import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
