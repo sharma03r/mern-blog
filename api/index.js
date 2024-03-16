64af7ed58a0d3880715b1bb918144b5e0576eaae
Rahul Sharma <sharmarahul8848@gmail.com>
2024-03-16 18:54:28 +0530

    Completed the backend for update user

--- api/index.js
+++ api/index.js
@@ -3,6 +3,9 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import userRoutes from "./routes/user.route.js";
+
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -15,12 +18,13 @@ mongoose
   .catch((err) => {
     console.log(`Error ${err}`);
   });
-
+app.use(cookieParser());
 app.listen(3000, () => {
   console.log(`Server running on port 3000`);
 });
 app.use(cors());
 app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal server error";
