c6e2a45fbaa3ce965bd1f7f4ff51b437ca3b43ca
Rahul Sharma <sharmarahul8848@gmail.com>
2024-03-24 13:08:23 +0530

    Created get user api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -12,5 +12,6 @@ router.get("/test", testUser);
 router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
 router.post("/signout", signout);
+router.get("/getusers", verifyToken, getUsers);
 
 export default router;
