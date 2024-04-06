13b27db90db566ea752aa65ad816c0af386ad9b0
Rahul Sharma <sharmarahul8848@gmail.com>
2024-04-07 02:27:36 +0530

    Added pre-installation

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
+import path from "path";
 
 dotenv.config();
 const app = express();
@@ -20,6 +21,7 @@ mongoose
   .catch((err) => {
     console.log(`Error ${err}`);
   });
+const __dirname = path.resolve();
 app.use(cookieParser());
 app.listen(3000, () => {
   console.log(`Server running on port 3000`);
@@ -29,6 +31,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/dist/index.html"));
+});
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal server error";
