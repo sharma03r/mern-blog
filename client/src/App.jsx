3de2047657f2a45b51a26826e1991d00d91b0d81
Rahul Sharma <sharmarahul8848@gmail.com>
2024-04-07 02:35:38 +0530

    removed about page imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signin from "./pages/Signin";
 import Home from "./pages/Home";
-import About from "./pages/About";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
@@ -22,7 +21,6 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<Signin />} />
         <Route path="/sign-up" element={<Signup />} />
         <Route path="/search" element={<Search />} />
